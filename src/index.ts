e22add94b318b33f644734e21f683bba023cedc8
Александр Львов <79278149+jullic@users.noreply.github.com>
2023-02-20 15:10:57 +0300

    index file

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export {
+	GlobalScene,
+	Filter,
+	IGlobalAction,
+	IGlobalCommand,
+	IGlobalHears,
+	IGlobalOn,
+	IGlobalSceneOptions,
+} from './scene/global-scene';
+export { GlobalStage } from './stage/global-stage';
+export {
+	GlobalSceneContextScene,
+	IGlobalSceneContext,
+	IGlobalSceneSession,
+} from './context/global-context';
